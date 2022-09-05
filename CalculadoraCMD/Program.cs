f816bd6f8aba3725e76698c218ab2d5ee9ba84ea
Andre Lopes <andrelneres@hotmail.com>
2022-09-04 21:13:13 -0300

    Criação do Repositório

---
+++ CalculadoraCMD/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculadoraCMD
+{
+    enum Menu { Soma = 1, Subtração, Divisao, Multiplicacao, Potencia, Raiz, Sair}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+
+            while (!escolheuSair) 
+            {
+                Console.WriteLine("SEJA BEM VINDO! VAMOS CALCULAR, SELECIONE UMA DAS OPÇÕES: ");
+                Console.WriteLine("1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n5-Potência\n6-Raiz\n7-Sair");
+
+                Menu opcao = (Menu)int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case Menu.Soma:
+                        Soma();
+                        break;  
+                    case Menu.Sair:
+                        escolheuSair |= true;
+                        break;
+                }
+                Console.Clear();
+            }
+
+        } 
+        
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("SOMA DE DOIS NÚMEROS");
+            Console.WriteLine("Digite o primeiro número: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            int b = int.Parse(Console.ReadLine());
+            int soma = a + b;
+            Console.WriteLine($"O resultado da soma de {a} + {b} é: {soma}");
+            Console.WriteLine("\nAperte ENTER para voltar ao MENU");
+            Console.ReadLine();
+        }
+    }
+}
